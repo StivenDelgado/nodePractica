936fd8b3657b67c0bb7ae12e73fdaeed334cff94
StivenDelgado <delgadostiven123@gmail.com>
2025-01-25 15:32:47 -0500

    arreglos

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,16 +1,15 @@
-import { promises as fs } from 'fs';
-console.log("hola");
+const fs = require('fs').promises;
+
 async function crearArchivo(nombre, contenido) {
+  fs.mkdir(`archivos`);
   return fs.writeFile(`archivos/${nombre}.txt`, contenido)
     .then(() => `Archivo ${nombre}.txt creado`)
     .catch((error) => console.error(`Error al crear ${nombre}.txt:`, error));
 }
 
 let arregloPromesas = [];
-let cantidad = parseInt(prompt("¿Cuántos archivos quieres crear?"))
 
-for (let index = 1; index <= cantidad; index++) {
+for (let index = 1; index <= 100; index++) {
   arregloPromesas.push(crearArchivo(String(index), "Hola VankVersity"))
 }
-
 Promise.all(arregloPromesas)
